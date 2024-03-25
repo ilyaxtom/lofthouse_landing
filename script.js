4577f64e75f7f748891ddc92051c9a5c15d3ed1f
ilyaxtom <xtom38@gmail.com>
2024-03-25 23:54:48 +0300

    phoneMask and youtubeLightbox

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const navIcon = document.querySelector(".nav-icon");
 navBtn.addEventListener("click", () => {
     navIcon.classList.toggle("nav-icon--active");
 })
+
+/* Phone mask */
+mask('[input-tel]');
+
+const phoneInputs = document.querySelectorAll('[input-tel]');
+
+phoneInputs.forEach((input) => {
+    input.addEventListener('input', () => {
+        if (input.value == '+') input.value = '';
+    })
+    input.addEventListener('blur', () => {
+        if (input.value == '+') input.value = '';
+    })
+})
